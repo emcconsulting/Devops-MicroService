6feb763ad0716c36d85b1e5c8d75cb922e1ffe46
santhosh M S <sanms1993@gmail.com>
2017-03-01 11:17:11 +0530

    to check ci-cd

--- src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
+++ src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Web V3.2").description("Web V3.2").termsOfServiceUrl("")
+		return new ApiInfoBuilder().title("Web V3.3").description("Web V3.3").termsOfServiceUrl("")
 				.contact("").license("Apache License Version 3.0").licenseUrl("").version("3.0").build();
 	}
 }
