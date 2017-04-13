83673b103a7946fd2b2975f70fa426453e0abd7d
santhosh M S <sanms1993@gmail.com>
2017-04-13 10:31:14 +0530

    Update SwaggerConfig.java

--- src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
+++ src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Web V4.4").description("Web V4.4").termsOfServiceUrl("")
+		return new ApiInfoBuilder().title("Web V4.4").description("Web V4.5").termsOfServiceUrl("")
 				.contact("").license("Apache License Version 3.0").licenseUrl("").version("3.0").build();
 	}
 }
