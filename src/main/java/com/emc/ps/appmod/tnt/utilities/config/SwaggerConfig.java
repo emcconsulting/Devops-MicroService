90b56331690ad08db2e847f47cac7b63957d9928
kchouhan <krishana.chouhan@emc.com>
2017-04-24 08:38:40 +0530

    DPC-70 Version upgrade

--- src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
+++ src/main/java/com/emc/ps/appmod/tnt/utilities/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Web V4.6").description("Web V4.6").termsOfServiceUrl("")
+		return new ApiInfoBuilder().title("Web V4.7").description("Web V4.7").termsOfServiceUrl("")
 				.contact("").license("Apache License Version 3.0").licenseUrl("").version("3.0").build();
 	}
 }
